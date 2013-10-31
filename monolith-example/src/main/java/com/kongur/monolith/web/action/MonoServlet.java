5f2089b1399a3972ec5ed0efd71e672630e40152
zhengwei <zheng19851@126.com>
2013-10-31 16:01:42 +0800

    create session-example
    create session-example

---
+++ monolith-example/src/main/java/com/kongur/monolith/web/action/MonoServlet.java
@@ -0,0 +1,46 @@
+package com.kongur.monolith.web.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+
+public class MonoServlet extends HttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -8327812623335190919L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+        System.out.println("in doget");
+
+        String username = (String) req.getSession().getAttribute("username");
+
+        if (StringUtils.isBlank(username)) {
+            System.out.println("no username");
+            req.getSession().setAttribute("username", "zhangsan");
+        } else {
+            System.out.println("username=" + username);
+        }
+
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+
+        System.out.println("in doPost");
+
+        super.doPost(req, resp);
+    }
+
+}
