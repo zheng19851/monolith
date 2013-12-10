74bf9845cc44f33cc71ab71227d87176dcd56e6b
zhengwei <zheng19851@126.com>
2013-12-10 10:23:48 +0800

    modify ExportExcelTool add export excel to HttpServeltResponse
    modify ExportExcelTool add export excel to HttpServeltResponse

--- monolith-common/src/main/java/com/kongur/monolith/common/poi/ExportExcelTool.java
+++ monolith-common/src/main/java/com/kongur/monolith/common/poi/ExportExcelTool.java
@@ -3,6 +3,8 @@ package com.kongur.monolith.common.poi;
 import java.io.OutputStream;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -19,27 +21,58 @@ public class ExportExcelTool {
     private static final Logger log = Logger.getLogger(ExportExcelTool.class);
 
     /**
+     * 导出excel到HttpServletResponse
+     * 
+     * @param fileName， 文件名称
+     * @param fields, 字段名称
+     * @param datas, 数据
+     * @param response, HttpServletResponse
+     * @param callback
+     * @return
+     * @throws ExportException
+     */
+    public static <T> boolean export(String fileName, String[] fields, List<T> datas, HttpServletResponse response,
+                                     ExportCallback<T> callback) throws ExportException {
+
+        boolean success = false;
+
+        try {
+            response.setHeader("Content-disposition", "attachment; filename="
+                                                      + new String(fileName.getBytes("GB2312"), "ISO8859-1"));
+            response.setContentType("application/msexcel");// 定义输出类型
+
+            success = export(fields, datas, response.getOutputStream(), callback);
+
+        } catch (Exception e) {
+            log.error("export excel error", e);
+            throw new ExportException(e);
+        }
+
+        return success;
+    }
+
+    /**
      * 导出excel
      * 
      * @param fields, 字段名称
      * @param datas, 数据
-     * @param out, 输出流
+     * @param out, 输出流，可以导出到文件，response等
      * @param callback
      * @return
      * @throws ExportException
      */
     public static <T> boolean export(String[] fields, List<T> datas, OutputStream out, ExportCallback<T> callback)
-                                                                                                            throws ExportException {
+                                                                                                                  throws ExportException {
         try {
             HSSFWorkbook workbook = new HSSFWorkbook();
             HSSFSheet sheet = workbook.createSheet();
             // 　workbook.setSheetName(0,sheetName,HSSFWorkbook.ENCODING_UTF_16);
 
-            if (fields != null) {
+            if (fields != null && fields.length > 0) {
                 HSSFRow row = sheet.createRow(0);
                 HSSFCell cell = null;
                 for (int i = 0; i < fields.length; i++) {
-                    cell = row.createCell((short)i);
+                    cell = row.createCell((short) i);
                     cell.setCellType(HSSFCell.CELL_TYPE_STRING);
                     // cell.setEncoding(HSSFCell.ENCODING_UTF_16);
                     // cell.setCellStyle(HSSFCellStyle.ALIGN_CENTER);
